3c0b06a9bfabbec85cb3cb0c70bb5e40b9c687bf
anton <anton061311@gmail.com>
2026-07-23 17:16:35 +0900

    feat: 2단계 완료

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,20 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String saying = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+
+            } else if(command.equals("종료")) {
+                break;
+            }
+        }
     }
 }
