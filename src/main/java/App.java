f10ea9e2a7345c3fb3ac8076a7e5467ca770d11e
anton <anton061311@gmail.com>
2026-07-23 17:38:53 +0900

    feat: Add wscontroller, systemconroller

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner scanner;
-    int lastId = 0;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
+    private WiseSayingController wiseSayingController;
+    private SystemController systemController = new SystemController();
 
     public App(Scanner scanner) {
         this.scanner = scanner;
+        this.wiseSayingController = new WiseSayingController(scanner);
     }
 
     public void run() {
@@ -20,28 +19,10 @@ public class App {
             String command = scanner.nextLine();
 
             switch (command) {
-                case "등록" -> {
-                    System.out.print("명언 : ");
-                    String saying = scanner.nextLine();
-                    System.out.print("작가 : ");
-                    String author = scanner.nextLine();
-
-                    lastId++;
-                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
-                }
+                case "등록" -> wiseSayingController.actionAdd();
+                case "목록" -> wiseSayingController.actionList();
                 case "종료" -> {
-                    return;
-                }
-                case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    wiseSayings
-                            .reversed()
-                            .stream()
-                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
-                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
-
+                    systemController.actionExit();
                 }
             }
         }
