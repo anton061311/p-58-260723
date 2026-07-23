7fa850396f69b725faa4508eaabc6bb86d2f7f58
anton <anton061311@gmail.com>
2026-07-23 17:25:17 +0900

    fea: t4() 추가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
 
     private Scanner scanner;
+    int lastId = 0;
 
     public App(Scanner scanner) {
         this.scanner = scanner;
@@ -21,6 +22,9 @@ public class App {
                     String saying = scanner.nextLine();
                     System.out.print("작가 : ");
                     String author = scanner.nextLine();
+
+                    lastId++;
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
                 }
                 case "종료" -> {
                     return;
