d620168de2181c5dd3e4a574bcd9c6c6cb3eed7c
anton <anton061311@gmail.com>
2026-07-24 12:37:29 +0900

    refactor: introduce PageDto for pagination

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -1,5 +1,6 @@
 package com.back.domain.wiseSaying.repository;
 
+import com.back.domain.wiseSaying.dto.PageDto;
 import com.back.domain.wiseSaying.entity.WiseSaying;
 
 import java.util.ArrayList;
@@ -35,24 +36,41 @@ public class WiseSayingRepository {
         return wiseSayings.reversed();
     }
 
-    public List<WiseSaying> findByContentContainingIdDesc(String keyword, int pageSize, int page) {        return wiseSayings
+    public PageDto findByContentContainingIdDesc(String keyword, int pageSize, int page) {
+
+        List<WiseSaying> result = wiseSayings
                 .reversed()
                 .stream()
                 .filter(
                         w -> w.getContent().contains(keyword)
                 )
-                .limit(pageSize)
                 .toList();
+
+        return pageOf(result, page, pageSize);
     }
 
-    public List<WiseSaying> findByAuthorContainingIdDesc(String keyword, int pageSize, int page) {        return wiseSayings
+    public PageDto findByAuthorContainingIdDesc(String keyword, int pageSize, int page) {
+        List<WiseSaying> result = wiseSayings
                 .reversed()
                 .stream()
                 .filter(
                         w -> w.getAuthor().contains(keyword)
                 )
-                .skip((page - 1) * pageSize)
+                .toList();
+
+        return pageOf(result, page, pageSize);
+    }
+    private PageDto pageOf(List<WiseSaying> filteredContent, int pageNo, int pageSize) {
+
+        List<WiseSaying> content = filteredContent.stream()
+                .skip((pageNo-1) * pageSize)
                 .limit(pageSize)
                 .toList();
+
+        int totalItems = filteredContent.size();
+
+        return new PageDto(pageNo, pageSize, totalItems, content);
     }
+
+
 }
